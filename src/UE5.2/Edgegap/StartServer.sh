262bb2c310d7f3a2a9748794c2c64643607f539d
orcist <motyl.kubo@gmail.com>
2024-06-25 17:37:49 +0200

    Update StartServer.sh

--- src/UE5.2/Edgegap/StartServer.sh
+++ src/UE5.2/Edgegap/StartServer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-GAME_PORT=$(echo $ARBITRIUM_PORTS_MAPPING | jq '.ports.gameport.external')
+GAME_PORT=$(echo $ARBITRIUM_PORTS_MAPPING | jq '.ports.gameport.internal')
 
 $(dirname "$0")/<PROJECT_NAME>Server.sh -log -PORT=$GAME_PORT 
