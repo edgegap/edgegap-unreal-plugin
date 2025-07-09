60356567ae1f37cc97bca1c86008612f21559716
orcist <motyl.kubo@gmail.com>
2025-07-09 16:53:56 -0400

    remove SIGKILL trap and fix ubuntu compatibility

--- src/UE5.2/Edgegap/StartServer.sh
+++ src/UE5.2/Edgegap/StartServer.sh
@@ -1,57 +1,51 @@
-#!/bin/sh
+#!/bin/bash
+
+pid=$$
+EXECUTABLE_PATH="$(dirname "$0")"
+: ${TARGET_FILE_NAME:="LyraServer"} # edit to match your .target.cs file name
 
 # variable and function definitions
-pid=$$
-GAME_PORT=$(echo $ARBITRIUM_PORTS_MAPPING | jq '.ports.gameport.internal')
-
 separator() {
   printf "%0.s~" {1..80}
 }
 
-catch_kill() {
-  echo "SIGKILL signal received..."
-  separator()
-  kill -KILL "$pid" 2>/dev/null
-}
-
 catch_term() {
-  echo "SIGTERM signal received..."
-  separator()
+  echo "TERM signal received..."
+  separator
   kill -TERM "$pid" 2>/dev/null
 }
 
 catch_quit() {
-  echo "SIGTERM signal received..."
-  separator()
+  echo "QUIT signal received..."
+  separator
   kill -QUIT "$pid" 2>/dev/null
 }
 
 # custom handling of process termination
-trap catch_term SIGTERM
-trap catch_kill SIGKILL
-trap catch_quit SIGQUIT
+trap catch_term TERM
+trap catch_quit QUIT
 
 # print variables for debugging purposes
-separator()
+separator
 echo ARBITRIUM_PORTS_MAPPING:
 echo "$ARBITRIUM_PORTS_MAPPING"
 
-separator()
+separator
 echo ENVIRONMENT VARS:
 env
 
 # initialize game server
-separator()
-echo Execute command: $(dirname "$0")/<PROJECT_NAME>Server.sh -log -PORT=$GAME_PORT $UE_COMMANDLINE_ARGS
-separator()
-$(dirname "$0")/<PROJECT_NAME>Server.sh -log -PORT=$GAME_PORT $UE_COMMANDLINE_ARGS
+separator
+echo Execute command: $EXECUTABLE_PATH/$TARGET_FILE_NAME.sh -log -PORT=$ARBITRIUM_PORT_GAMEPORT_INTERNAL $UE_COMMANDLINE_ARGS
+separator
+$EXECUTABLE_PATH/$TARGET_FILE_NAME.sh -log -PORT=$ARBITRIUM_PORT_GAMEPORT_INTERNAL $UE_COMMANDLINE_ARGS
 
 # after game server process terminates
-separator()
+separator
 echo Gameserver exit code: $?
 
 # if server terminated terminated from Unreal, stop deployment
-separator()
+separator
 echo Execute command: curl -s -X DELETE -H "Authorization: ${ARBITRIUM_DELETE_TOKEN}" "${ARBITRIUM_DELETE_URL}" | jq -r '.'
 curl -s -X DELETE -H "Authorization: ${ARBITRIUM_DELETE_TOKEN}" "${ARBITRIUM_DELETE_URL}" | jq -r '.'
 sleep infinity
